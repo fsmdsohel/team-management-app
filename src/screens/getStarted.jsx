461e3691faf328df0c8b507df782018b80f2dd4c
fsmdsohel <sohel.rana.coder@gmail.com>
2022-04-10 13:43:56 +0600

    get started screen added

--- src/screens/getStarted.jsx
+++ src/screens/getStarted.jsx
@@ -16,22 +16,37 @@ const GetStarted = () => {
   return (
     <View style={style.container}>
       <View style={style.wrapper}>
-        <View style={{height: windowHeight / 2.6, width: windowWidth}}>
+        <View
+          style={{
+            height: windowHeight / 2.6,
+            width: windowWidth,
+            marginBottom: 20,
+          }}>
           <LottieView source={lottie} autoPlay loop />
         </View>
         <View style={style.body}>
           <Text style={style.subLabel}>Task Management</Text>
-          <View style={{flexDirection: "row"}}>
-            <Text style={style.label}>
-              Let's Create a Space For Your Workflows.
-            </Text>
-            <TouchableOpacity>
-              <View>
-                <Text>
-                  <Icon name="arrow-right" size={30} color="#000" />
-                </Text>
+          <View style={style.bodyWrapper}>
+            <View style={{flex: 1.5}}>
+              <Text style={style.label}>
+                Let's Create a Space For Your Workflows.
+              </Text>
+              <TouchableOpacity>
+                <View style={style.button}>
+                  <Text style={style.buttonText}>Get Start</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={style.boxWrapper}>
+              <View style={{position: "relative", right: -100}}>
+                <TouchableOpacity>
+                  <View style={style.box} />
+                  <View style={style.arrowRight}>
+                    <Icon name="arrow-right" size={30} color="#171a20" />
+                  </View>
+                </TouchableOpacity>
               </View>
-            </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -51,16 +66,53 @@ const styles = (theme) =>
     },
     wrapper: {},
     body: {
-      padding: 15,
+      padding: 20,
     },
     subLabel: {
       color: theme.colors.PRIMARY,
       fontFamily: "Lato-Bold",
       fontSize: 16,
+      marginBottom: 20,
     },
     label: {
       fontFamily: "Lato-Bold",
-      fontSize: 36,
+      fontSize: 38,
       color: "#1e1e1e",
+    },
+    bodyWrapper: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    boxWrapper: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    box: {
+      width: 150,
+      height: 150,
+      margin: 20,
+      borderRadius: 40,
+      backgroundColor: "#e4ecff",
+      transform: [{rotate: "40deg"}],
+    },
+    button: {
+      backgroundColor: theme.colors.PRIMARY,
+      alignItems: "center",
+      width: 130,
+      borderRadius: 10,
+      paddingVertical: 12,
+      marginVertical: 40,
+    },
+    arrowRight: {
+      position: "absolute",
+      top: "50%",
+      left: 40,
+      transform: [{translateY: -14}],
+    },
+    buttonText: {
+      fontFamily: "Lato-Bold",
+      color: "#fff",
+      fontSize: 16,
     },
   });
