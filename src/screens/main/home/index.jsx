3bfa53e826fc5207a6ca908651673675a77c702c
fsmdsohel <sohel.rana.coder@gmail.com>
2022-04-16 00:53:58 +0600

    home screen 40% complete

--- src/screens/main/home/index.jsx
+++ src/screens/main/home/index.jsx
@@ -1,31 +1,14 @@
 import React from "react";
 import {StyleSheet, Text, View, Button} from "react-native";
-import {useNavigation} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {Modal} from "components";
+import Home from "./home";
 
 const Stack = createNativeStackNavigator();
 
-const Home = () => {
-  const navigation = useNavigation();
-  return (
-    <View>
-      <Text>hello from notification</Text>
-      <Button
-        onPress={() => navigation.navigate("nestedNotifications")}
-        title="Notification"
-      />
-      <Modal />
-    </View>
-  );
-};
 const Notifications = () => {
-  const navigation = useNavigation();
-
   return (
     <View>
       <Text>hello from home</Text>
-      <Button onPress={() => navigation.navigate("nestedHome")} title="Home" />
     </View>
   );
 };
